dd004032c381af3717882e399dd20d1bf91b5e78
hakan009 <hakanalifpramudya13@gmail.com>
2023-10-29 22:03:56 +0700

    Progress 2 - UTS

--- UTS-Hakan/uts_hakan_flutter/lib/main.dart
+++ UTS-Hakan/uts_hakan_flutter/lib/main.dart
@@ -12,49 +12,134 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(5.0),
-                  margin: const EdgeInsets.only(top: 20.0, right:  20.0),
-                  width: 120,
-                  child: Image(
-                    image:NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToIhy4KyY-ALuwCR9Z3_zCTW--fU_3agJjOItWx2hLBA&s'),
-                    width: 36,
-                    height: 36,
-                    fit: BoxFit.scaleDown,),
+        body: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.only(right: 20.0),
+                child: Image(
+                  image: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToIhy4KyY-ALuwCR9Z3_zCTW--fU_3agJjOItWx2hLBA&s'),
+                  width: 36,
+                  height: 36,
+                  fit: BoxFit.scaleDown,
                 ),
-                SizedBox(width: 10),
-                Icon(Icons.airplane_ticket, size: 36,),
-                SizedBox(width: 10),
-                Icon(Icons.heart_broken, size: 36,),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child : DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      border: Border.all(color: Colors.red, width: 2.0),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      child: Center(
-                        child: Text('Decoration Box'),
-                      ),
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 36, // Set the height of the Icon
+                    child: Icon(
+                      Icons.airplane_ticket,
+                      size: 36,
                     ),
                   ),
+                  Container(
+                    width: 36, // Set the width of the Icon
+                    height: 36, // Set the height of the Icon
+                    child: Icon(
+                      Icons.heart_broken,
+                      size: 36,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              border: Border.all(color: Colors.red, width: 2.0),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Hi, Hakan Alif Pramudya',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 5.0),
+                      padding: EdgeInsets.all(10.0),
+                      width: 150,
+                      height: 75,
+                      decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Your Balance',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                'Rp. 10.000',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold, 
+                                ),
+                              ),
+                              Icon(Icons.arrow_forward),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                  padding: EdgeInsets.all(10.0),
+                  width: 150,
+                  height: 75,
+                  decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.white, width: 2.0),
+                  borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Bonus Balance',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '0',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold, 
+                            ),
+                          ),
+                          Icon(Icons.arrow_forward),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+                  ],
                 ),
               ],
-            )
-          ]
-        ),
+            ),
+          )
+        ]),
       ),
     );
   }
